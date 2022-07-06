041f4f8815e2e26aa2666c53ec135cd07d53b3c2
IRSAGE <sibo.egide98@gmail.com>
2022-07-06 21:33:01 +0200

    Instaling and configuring the navigation system

--- App.js
+++ App.js
@@ -1,15 +1,21 @@
+import 'react-native-gesture-handler';
 import React from 'react';
-import {View} from 'react-native';
 import Home from './screens/Home';
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import Details from './screens/Details';
+
+const Stack = createStackNavigator();
 
 const App = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-      }}>
-      <Home />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Details" component={Details} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 export default App;
